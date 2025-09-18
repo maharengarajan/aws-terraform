71be6b9f2462802b7b166605039d3ab24122a8c1
maharengarajan <maharengarajan46@gmail.com>
2025-09-18 11:14:42 +0530

    vpc cidr variables added

--- variables.tf
+++ variables.tf
@@ -15,3 +15,22 @@ variable "environment" {
     type        = string
     default     = "dev"
 }
+
+variable "vpc_cidr" {
+    description = "VPC CIDR block"
+    type        = string
+    default     = "10.0.0.0/16"  # VPC has 65,536 private IPs to allocate
+}
+
+variable "private_subnet_cidrs" {
+    description = "List of private subnet CIDR blocks"
+    type        = list(string)
+    default     = ["10.0.1.0/24", "10.0.2.0/24"]  # Each /24 subnet has 256 IPs
+}
+
+variable "instance_type" {
+    description = "EC2 instance type"
+    type        = string
+    default     = "t3.micro"  
+}
+
