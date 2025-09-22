c07dc41c9ae4ca03f4477f602640108e077729d7
maharengarajan <maharengarajan46@gmail.com>
2025-09-22 16:27:28 +0530

    variable added

--- variables.tf
+++ variables.tf
@@ -7,3 +7,14 @@ variable "project_name" {
   description = "The name of the project."
   type        = string  
 }
+
+variable "instance_type" {
+  description = "The type of EC2 instance to use."
+  type        = string
+  default     = "t3.micro"  
+}
+
+variable "key_pair_name" {
+  description = "The name of the key pair to use for EC2 instances."
+  type        = string  
+}
